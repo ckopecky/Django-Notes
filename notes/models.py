7217b9dacb0d5da070a16856368a55c7d650dcba
ckopecky <ckopecky@github.com>
2018-09-23 10:39:34 -0700

    Create personal note class
    Works

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from uuid import uuid4 
 
@@ -6,4 +7,15 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title=models.CharField(max_length=140)
     content=models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
+    def __str__ (self):
+        return self.title
+
+    class Meta:
+        ordering= ('title',)
+
+
+class PersonalNote(Note):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
